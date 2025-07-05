5088a0deeca87c57c0752d02f19c2419036c580a
vly1vly1 <lyyangvictor@gmail.com>
2025-07-05 19:09:12 +0200

    Component blog creado y añadiro a app.html

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { BlogComponent } from "./components/blog/blog.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [BlogComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
